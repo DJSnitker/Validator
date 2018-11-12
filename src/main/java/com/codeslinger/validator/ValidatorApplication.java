d6a342c84414a71826c94f6e5198e98d03c5ea09
Douglas Snitker <dougsnitker@gmail.com>
2018-11-11 20:14:55 -0600

    Refactoring Package Structure

--- src/main/java/com/codeslinger/validator/ValidatorApplication.java
+++ src/main/java/com/codeslinger/validator/ValidatorApplication.java
@@ -1,4 +1,4 @@
-package com.codeslinger.Validator;
+package com.codeslinger.validator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
