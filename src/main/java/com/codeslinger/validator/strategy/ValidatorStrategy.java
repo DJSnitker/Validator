785319e61657f6c754a4aa4f56935e90f7c3f4b2
Douglas Snitker <dougsnitker@gmail.com>
2018-11-19 23:35:19 -0600

    Added unit tests and modified strategy to be null safe

--- src/main/java/com/codeslinger/validator/strategy/ValidatorStrategy.java
+++ src/main/java/com/codeslinger/validator/strategy/ValidatorStrategy.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 
 @Service("Strategy")
 @Scope("prototype")
@@ -19,7 +20,11 @@ public class ValidatorStrategy implements Validator {
 
     @Override
     public boolean validate(Validation targetValidation){
-        Validator val = (Validator) ctx.getBean(targetValidation.getType());
-        return val.validate(targetValidation);
+        if(!StringUtils.isBlank(targetValidation.getType())) {
+            Validator val = (Validator) ctx.getBean(targetValidation.getType());
+            return val.validate(targetValidation);
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/com/codeslinger/validator/controller/ValidationControllerTest.java
+++ src/test/java/com/codeslinger/validator/controller/ValidationControllerTest.java
@@ -3,6 +3,7 @@ package com.codeslinger.validator.controller;
 import com.codeslinger.validator.resources.Validation;
 import com.codeslinger.validator.strategy.MockFalseValidationStrategy;
 import com.codeslinger.validator.strategy.MockTrueValidationStrategy;
+import com.codeslinger.validator.strategy.ValidatorStrategy;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,11 +15,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ValidationControllerTest {
 
     @Test
+    public void badValidator() {
+        final String expectedResponse = "NOT OK";
+        ValidationController vc = new ValidationController();
+        vc.setValidator( new ValidatorStrategy() );
+        Validation testValidation = new Validation(null, "NAME", "STUFF");
+        String actualResponse = vc.validate(testValidation);
+        Assert.assertEquals("assert NOT OK is returned", expectedResponse, actualResponse);
+    }
+
+    @Test
     public void validate() {
         final String expectedResponse = "OK";
         ValidationController vc = new ValidationController();
         vc.setValidator( new MockTrueValidationStrategy() );
-        Validation testValidation = new Validation("TYPE", "NAME", "STUFF");
+        Validation testValidation = new Validation("A", "NAME", "STUFF");
         String actualResponse = vc.validate(testValidation);
         Assert.assertEquals("assert OK is returned", expectedResponse, actualResponse);
     }
@@ -28,7 +39,7 @@ public class ValidationControllerTest {
         final String expectedResponse = "NOT OK";
         ValidationController vc = new ValidationController();
         vc.setValidator( new MockFalseValidationStrategy() );
-        Validation testValidation = new Validation("TYPE", "NAME", "STUFF");
+        Validation testValidation = new Validation("A", "NAME", "STUFF");
         String actualResponse = vc.validate(testValidation);
         Assert.assertEquals("assert NOT OK is returned", expectedResponse, actualResponse);
     }
